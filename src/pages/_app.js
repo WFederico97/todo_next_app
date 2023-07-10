c3db3f6d7acbd4c663a3216bd3243ae754ae54f5
Federico W <wuthrich09@gmail.com>
2023-07-10 17:48:59 -0300

    Vuexy refactor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import AuthProvider from '@/context/Auth-Context'
+import AuthProvider from '@/context/AuthContext'
 
 export default function App({ Component, pageProps }) {
   return (
