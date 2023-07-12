c82711b40d69042070b79af8e8a745f662b1a2bb
Federico W <wuthrich09@gmail.com>
2023-07-12 20:47:34 -0300

    ADD Navbar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,3 +1,7 @@
+// **  React Imports
+import { useState } from 'react';
+
+
 // ** MUI imports
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
@@ -8,30 +12,45 @@ import Button from '@mui/material/Button';
 import ListItem from '@mui/material/ListItem';
 import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
-
-
+import LogoutIcon from '@mui/icons-material/Logout';
 
 // ** Next Imports
 import Link from 'next/link';
 
+import { useAuth } from '@/hooks/useAuth';
 
-export default function ButtonAppBar() {
+
+
+export default function ButtonAppBar(s) {
+
+
+    const {logOut, show} = useAuth()
+
+    const logOutHandler = () => {
+        logOut()
+    }
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <Grid xs={12} sm={12}>
-                        <List  sx={{display:'flex'}}>
+                    <Grid sx={{ display: 'flex', alignItems: "center" }}>
+                        <List sx={{ display: "inline-flex" }}  >
                             <ListItem >
-                                <Link style={{textDecoration: "none", color:"#fff"}}  href="/">Home</Link>
+                                <Link style={{ textDecoration: "none", color: "#fff" }} href="/">Home</Link>
                             </ListItem>
                             <ListItem>
-                                <Link style={{textDecoration: "none", color:"#fff"}} href="/register">Register</Link>
+                                <Link style={{ textDecoration: "none", color: "#fff" }} href="/register">Register</Link>
                             </ListItem>
                             <ListItem>
-                                <Link style={{textDecoration: "none", color:"#fff"}} href="/login">Login</Link>
+                                <Link style={{ textDecoration: "none", color: "#fff" }} href="/login">Login</Link>
                             </ListItem>
                         </List>
+                        {
+                            show && <Button onClick={logOutHandler} ><LogoutIcon  color='action' /></Button>
+                        }
+                    </Grid>
+                    <Grid>
                     </Grid>
                 </Toolbar>
             </AppBar>
